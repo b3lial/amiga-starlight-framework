7b14d97e6f7533608f3775f81228a7767a3cc8b5
Christian Ammann <christian.ammann@gmail.com>
2018-03-03 19:27:06 +0100

    adding wb 20 stuff

--- payload_intro.c
+++ payload_intro.c
@@ -116,8 +116,41 @@ void initPayloadIntro(void){
     //Init ViewPort
     InitVPort(&viewPort);
     viewPort.RasInfo = &rasInfo;
-    viewPort.DWidth  = width; //TODO: -1?
-    viewPort.DHeight = height; //TODO: -1?
+    viewPort.DWidth  = width; 
+    viewPort.DHeight = height;
+
+    //Make a ViewPortExtra and get ready to attach it
+    if(GfxBase->LibNode.lib_Version >= 36)
+    {
+        if( vpextra = GfxNew(VIEWPORT_EXTRA_TYPE) )
+        {
+            vcTags[1].ti_Data = (ULONG) vpextra;
+
+            /* Initialize the DisplayClip field of the ViewPortExtra */
+            if( GetDisplayInfoData( NULL , (UBYTE *) &dimquery ,
+                        sizeof(dimquery) , DTAG_DIMS, modeID) )
+            {
+                vpextra->DisplayClip = dimquery.Nominal;
+
+                /* Make a DisplayInfo and get ready to attach it */
+                if( !(vcTags[2].ti_Data = (ULONG) FindDisplayInfo(modeID)) )
+                    fail("Could not get DisplayInfo\n");
+                }
+            else
+            {
+                fail("Could not get DimensionInfo \n");
+            }
+        }
+        else{
+            fail("Could not get ViewPortExtra\n");
+        }
+
+        /* This is for backwards compatibility with, for example,   */
+        /* a 1.3 screen saver utility that looks at the Modes field */
+        viewPort.Modes = (UWORD) (modeID & 0x0000ffff);
+    }
+
+    //TODO: ...
 }
 
 void cleanBitPlanes(PLANEPTR* bmPlanes, UBYTE bmDepth, 
